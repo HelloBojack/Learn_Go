eb66c2f49cc3c77edd4b9e306759cbbe95e126d4
bojack <695668678@qq.com>
2021-05-26 14:32:23 +0800

    learn go

--- 05-Array&Slice&Map&Range/Array.go
+++ 05-Array&Slice&Map&Range/Array.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 
+// 数组作为参数 必须明确数组长度
+func arrFn(arr [5]int) {
+	// 且传参是值传递，不修改原数组
+	arr[4] = 1
+	for index, value := range arr {
+		fmt.Println(index, value)
+	}
+}
+
 func main() {
+	// 数组：固定长度
+	var arr [5]int
+	arr1 := [5]int{1, 2, 3}
 
-	var a [5]int
-	a1 := [3]int{1, 2, 3}
-	fmt.Println(a, a1)
-
+	arrFn(arr)
+	arrFn(arr1)
+	fmt.Println(arr)
 }
--- 05-Array&Slice&Map&Range/Slices.go
+++ 05-Array&Slice&Map&Range/Slices.go
@@ -2,14 +2,25 @@ package main
 
 import "fmt"
 
+// slice 切片 动态数组 可以动态长度 函数传递为引用传递 可直接修改原数组
+func sliceFn(s []string) {
+	s[2] = "200"
+	for _, v := range s {
+		fmt.Println(v)
+	}
+}
+
 func main() {
 	s := make([]string, 3)
 	s[0] = "0"
 	s = append(s, "1")
 
 	s2 := []string{"1", "2", "3"}
+	sliceFn(s2)
+
 	s3 := make([]string, len(s2))
 	copy(s3, s2)
+
 	twoDs := make([][]string, 3)
 	twoDs[0] = s
 	twoDs[1] = s2
