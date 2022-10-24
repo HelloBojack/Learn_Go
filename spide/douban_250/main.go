fb7f4871307291a99bcc68ee6257a1c601c3adbd
bojack <xlk970115@gmail.com>
2022-10-24 13:38:01 +0800

    spider

--- spide/douban_250/main.go
+++ spide/douban_250/main.go
@@ -1,18 +1,57 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/PuerkitoBio/goquery"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func main() {
-	Spider()
+type MovieInfo struct {
+	Index             int    `json:"index"`
+	Title             string `json:"title"`
+	Rating_num        string `json:"rating_num"`
+	Rating_people_num string `json:"rating_people_num"`
 }
 
-func Spider() {
+func main() {
+	var (
+		client     = InitMogDB()
+		collection *mongo.Collection
+	)
+	collection = client.Database("my_db").Collection("douban_250")
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("正在爬取第 %d 页", i)
+		Spider(strconv.Itoa(i*25), collection)
+	}
+}
+
+func InitMogDB() *mongo.Client {
+	var (
+		client *mongo.Client
+		err    error
+	)
+	//1.建立连接
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	return client
+}
+
+func Spider(pageNumber string, collection *mongo.Collection) {
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://movie.douban.com/top250", nil)
+	req, err := http.NewRequest("GET", "https://movie.douban.com/top250?start="+pageNumber, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -34,9 +73,29 @@ func Spider() {
 	}
 	defer resp.Body.Close()
 
-	res, err := ioutil.ReadAll(resp.Body)
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(res))
+	doc.Find("#wrapper #content .article .grid_view .item ").Each(func(i int, s *goquery.Selection) {
+		pageNumberInt, _ := strconv.Atoi(pageNumber)
+		index := pageNumberInt + i + 1
+		title := s.Find(".hd .title:nth-child(1)").Text()
+		rating_num := s.Find(".bd .star .rating_num").Text()
+		rating_people_num := s.Find(".bd .star span:nth-child(4)").Text()
+
+		movieInfo := MovieInfo{
+			Index:             index,
+			Title:             title,
+			Rating_num:        rating_num,
+			Rating_people_num: rating_people_num,
+		}
+		bsonMovieInfo := bson.M{"index": movieInfo.Index, "title": movieInfo.Title, "rating_num": movieInfo.Rating_num, "rating_people_num": movieInfo.Rating_people_num}
+		_, err := collection.InsertOne(context.TODO(), bsonMovieInfo)
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		// fmt.Println(result.InsertedID)
+	})
 }
